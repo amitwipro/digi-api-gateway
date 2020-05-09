7f38f50499a2ed65d1ed49adb4d7a6f1018158dc
amitaucs <the.amisoft@gmail.com>
2020-05-09 17:56:30 +0200

    Configured filtre

--- src/main/java/com/digi/apigateway/filter/AccountPreFilter.java
+++ src/main/java/com/digi/apigateway/filter/AccountPreFilter.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
+import java.util.Base64;
+
 @Slf4j
 @Component
 public class AccountPreFilter extends AbstractGatewayFilterFactory<AccountPreFilter.Config> {
@@ -19,11 +21,16 @@ public class AccountPreFilter extends AbstractGatewayFilterFactory<AccountPreFil
     @Override
     public GatewayFilter apply(Config config) {
 
+        String auth = "digi" + ":" + "digi";
+        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes());
+        String basic = "Basic "+encodedAuth;
+
         log.info("Inside Account Pre Filter");
         return (exchange, chain) -> {
             log.info("Intercepted request to account");
             ServerHttpRequest request = exchange.getRequest().mutate()
-                    .header("account", "Intercepted account").build();
+                    .header("Authorization", basic)
+                    .build();
             return chain.filter(exchange.mutate().request(request).build());
         };
     }
